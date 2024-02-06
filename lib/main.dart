e5c4e5ee1078b66f2e9e4756ed76e6fed09f0f4c
Jamie Walker <jamie@jamiewalker.nz>
2024-02-06 13:53:06 +1300

    Integrates the app with Google Firebase
    The Firebase configuration files are .gitignored

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,18 @@
 import 'package:easy_localization/easy_localization.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:jamie_walker_website/app/jamie_walker_router_config.dart';
 import 'package:jamie_walker_website/app/localization/app_locales.dart';
 import 'package:jamie_walker_website/app/theme/custom_theme.dart';
+import 'package:jamie_walker_website/firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await EasyLocalization.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   runApp(
     EasyLocalization(
