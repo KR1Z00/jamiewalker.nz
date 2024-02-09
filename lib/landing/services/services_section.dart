84cc0894dbd3ce244334a33ea6ed3ce17485e39a
Jamie Walker <jamie@jamiewalker.nz>
2024-02-09 14:41:38 +1300

    Refactors the ServicesSection to be a public class instead of a part of the LandingPage

--- lib/landing/services/services_section.dart
+++ lib/landing/services/services_section.dart
@@ -1,11 +1,22 @@
-part of '../view/landing_page.dart';
+import 'package:collection/collection.dart';
+import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/material.dart';
+import 'package:jamie_walker_website/app/extensions/screen_size.dart';
+import 'package:jamie_walker_website/app/localization/generated/locale_keys.g.dart';
+import 'package:jamie_walker_website/app/localization/json_list_translation.dart';
+import 'package:jamie_walker_website/app/theme/custom_colors.dart';
+import 'package:jamie_walker_website/app/theme/custom_text_styles.dart';
+import 'package:jamie_walker_website/generic/view/wrapping_cards_section.dart';
+import 'package:jamie_walker_website/landing/services/jw_service.dart';
 
-class _ServicesSection extends StatelessWidget {
+part 'service_card.dart';
+
+class ServicesSection extends StatelessWidget {
   static const double cardSpacing = 40;
   static const double minimumCardWidth = 340;
   static const int maximumCardsPerRow = 3;
 
-  const _ServicesSection();
+  const ServicesSection({super.key});
 
   @override
   Widget build(BuildContext context) {
