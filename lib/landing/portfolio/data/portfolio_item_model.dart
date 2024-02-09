13d42ead769dbe3f1648077886e9be0f76966df4
Jamie Walker <jamie@jamiewalker.nz>
2024-02-09 16:07:18 +1300

    Adds a new PortfolioItemModel that represents an item in the portfolio

---
+++ lib/landing/portfolio/data/portfolio_item_model.dart
@@ -0,0 +1,38 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'portfolio_item_model.freezed.dart';
+part 'portfolio_item_model.g.dart';
+
+@freezed
+class PortfolioItemModel with _$PortfolioItemModel {
+  factory PortfolioItemModel({
+    required String itemId,
+    required String name,
+    required String previewImageAsset,
+    required List<PortfolioItemInfoPage> infoPages,
+  }) = _PortfolioItemModel;
+
+  factory PortfolioItemModel.fromJson(Map<String, dynamic> json) =>
+      _$PortfolioItemModelFromJson(json);
+}
+
+@freezed
+class PortfolioItemInfoPage with _$PortfolioItemInfoPage {
+  factory PortfolioItemInfoPage({
+    required PortfolioItemInfoPageContentType contentType,
+    String? imageAsset,
+    String? youtubeVideoId,
+    required String description,
+  }) = _PortfolioItemInfoPage;
+
+  factory PortfolioItemInfoPage.fromJson(Map<String, dynamic> json) =>
+      _$PortfolioItemInfoPageFromJson(json);
+}
+
+@JsonEnum()
+enum PortfolioItemInfoPageContentType {
+  @JsonValue('image')
+  image,
+  @JsonValue('video')
+  video,
+}
