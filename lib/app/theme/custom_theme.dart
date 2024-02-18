53c487c2c2b7039ca9a72e939c4905f6150eda59
Jamie Walker <jamie@jamiewalker.nz>
2024-02-18 23:34:53 +1300

    Injects the customTextTheme into the app's customThemeData
    The ThemeData and TextTheme both now depend on the BuildContext. This allows us to modify them for mobile etc.

--- lib/app/theme/custom_theme.dart
+++ lib/app/theme/custom_theme.dart
@@ -1,13 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:jamie_walker_website/app/theme/text_theme.dart';
 import 'custom_colors.dart';
 
-final ThemeData customThemeData = ThemeData(
-  colorScheme: ColorScheme.fromSeed(
-      seedColor: CustomColors.primaryColor, brightness: Brightness.dark),
-  scrollbarTheme: ScrollbarThemeData(
-    thumbColor: MaterialStateProperty.all(
-      Colors.white.withOpacity(0.7),
-    ),
-  ),
-  useMaterial3: true,
-);
+ThemeData customThemeData(BuildContext context) => ThemeData(
+      colorScheme: ColorScheme.fromSeed(
+        seedColor: CustomColors.primaryColor,
+        brightness: Brightness.dark,
+      ),
+      scrollbarTheme: ScrollbarThemeData(
+        thumbColor: MaterialStateProperty.all(
+          Colors.white.withOpacity(0.7),
+        ),
+      ),
+      textTheme: customTextTheme(context),
+      useMaterial3: true,
+    );
