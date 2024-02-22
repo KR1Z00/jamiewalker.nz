eaea729f78f021826d4acfadfc50887b17aa8c7d
Jamie Walker <jamie@jamiewalker.nz>
2024-02-22 14:39:28 +1300

    Makes the PortfolioItemCard have a max width for its content
    This is to fix the image being overly huge on some screens

--- lib/landing/portfolio/view/portfolio_item_card.dart
+++ lib/landing/portfolio/view/portfolio_item_card.dart
@@ -5,7 +5,8 @@ import 'package:jamie_walker_website/app/theme/text_theme.dart';
 import 'package:jamie_walker_website/landing/portfolio/data/portfolio_item_model.dart';
 
 class PortfolioItemCard extends StatefulWidget {
-  static const double aspectRatio = 16 / 9;
+  static const double imageAspectRatio = 4 / 3;
+  static const double maxImageWidth = 500;
 
   final PortfolioItemModel model;
   final void Function() onTap;
@@ -35,54 +36,62 @@ class _PortfolioItemCardState extends State<PortfolioItemCard> {
       }),
       child: GestureDetector(
         onTap: widget.onTap,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            DecoratedBox(
-              decoration: BoxDecoration(
-                boxShadow: [
-                  StandardBoxShadows.regular(),
-                ],
-              ),
-              child: AspectRatio(
-                aspectRatio: 4 / 3,
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(10),
-                  child: Stack(
-                    fit: StackFit.expand,
-                    children: [
-                      FittedBox(
-                        fit: BoxFit.cover,
-                        clipBehavior: Clip.hardEdge,
-                        child: Image.asset(
-                          widget.model.previewImageAsset,
-                        ),
-                      ),
-                      AnimatedOpacity(
-                        opacity: _isHovered ? 1 : 0.3,
-                        duration: const Duration(milliseconds: 200),
-                        child: ColoredBox(
-                          color: Colors.white.withOpacity(0.3),
-                        ),
-                      ),
+        child: Align(
+          alignment: Alignment.center,
+          child: ConstrainedBox(
+            constraints: const BoxConstraints(
+              maxWidth: PortfolioItemCard.maxImageWidth,
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                DecoratedBox(
+                  decoration: BoxDecoration(
+                    boxShadow: [
+                      StandardBoxShadows.regular(),
                     ],
                   ),
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(
-                top: 20,
-              ),
-              child: Text(
-                widget.model.name,
-                style: context.textTheme().headlineMedium?.copyWith(
-                      color: context.colorScheme().secondary,
+                  child: AspectRatio(
+                    aspectRatio: PortfolioItemCard.imageAspectRatio,
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(10),
+                      child: Stack(
+                        fit: StackFit.expand,
+                        children: [
+                          FittedBox(
+                            fit: BoxFit.cover,
+                            clipBehavior: Clip.hardEdge,
+                            child: Image.asset(
+                              widget.model.previewImageAsset,
+                            ),
+                          ),
+                          AnimatedOpacity(
+                            opacity: _isHovered ? 1 : 0.3,
+                            duration: const Duration(milliseconds: 200),
+                            child: ColoredBox(
+                              color: Colors.white.withOpacity(0.3),
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
-              ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(
+                    top: 20,
+                  ),
+                  child: Text(
+                    widget.model.name,
+                    style: context.textTheme().headlineMedium?.copyWith(
+                          color: context.colorScheme().secondary,
+                        ),
+                  ),
+                ),
+              ],
             ),
-          ],
+          ),
         ),
       ),
     );
